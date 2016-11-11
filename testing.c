81194056ddae9b2a3ef6991c613626851aa9e921
cg14823 <cg14823@it025970.users.bris.ac.uk>
2016-11-11 16:48:15 +0000

    testing semisuccesfull

--- testing.c
+++ testing.c
@@ -49,69 +49,71 @@ int main(int argc, char* argv[]){
 
   sendbuf = (int*)malloc(sizeof(int) * local_ncols);
   recvbuf = (int*)malloc(sizeof(int) * local_ncols);
-  // devide grid between 4 threads
-  for(ii=0;ii<local_nrows;ii++) {
-    for(jj=0;jj<local_ncols;jj++) {
-      temp1[(ii+1) * NX +jj] = grid[(ii+rank) * NX +jj];
+  for (int it = 0; it < 3; it++){
+    // devide grid between 4 threads
+    for(ii=0;ii<local_nrows;ii++) {
+      for(jj=0;jj<local_ncols;jj++) {
+        temp1[(ii+1) * NX +jj] = grid[(ii+rank) * NX +jj];
+      }
     }
-  }
-  // copy data to be send left and right in this specific case
+    // copy data to be send left and right in this specific case
 
-  for (ii = 0; ii<local_ncols;ii++){
-    sendbuf[ii] = temp1[ NX +ii ];
-  }
-
-  // send data left and receive right
-
-  MPI_Sendrecv(sendbuf,4,MPI_INT,left,tag,
-              recvbuf,4,MPI_INT,right,tag,
-            MPI_COMM_WORLD,&status);
-  for (jj = 0; jj < local_ncols;jj++){
-    temp1[(local_nrows +1)*NX +jj] = recvbuf[jj];
-  }
-
-  // send data right receive left
-  MPI_Sendrecv(sendbuf,4,MPI_INT,right,tag,
-              recvbuf,4,MPI_INT,left, tag,
-              MPI_COMM_WORLD, &status);
-
-  for (jj = 0; jj < local_ncols;jj++){
-    temp1[jj] = recvbuf[jj];
-  }
-
-  // at this point all tanks have 3 rows so can do its calculation
-  // do calculation
-
-  for (jj = 0; jj <local_ncols;jj++){
-    temp1[NX +jj] = temp1[NX +jj] + temp1[jj] +temp1[NX*2 +jj];
-  }
-
-  // join grid again
-  for (ii = 0; ii< NX; ii++){
-    grid[rank*NX +ii] = temp1[NX +ii];
-  }
-  if (rank == 0){
-    for(jj =0; jj<local_ncols; jj++){
-      printf("%3d ",temp1[NX +jj]);
-      gridfinal[jj] = temp1[NX +jj];
-    }
-    printf("\n");
-    for(int k= 1; k <size;k++){
-      MPI_Recv(recvbuf,4,MPI_INT,k,tag,MPI_COMM_WORLD,&status);
-    	for(jj=0;jj < 4;jj++) {
-    	  printf("%3d ",recvbuf[jj]);
-        gridfinal[k*NX +jj] = recvbuf[jj];
-    	}
-      printf("\n");
-    }
-  }
-  else {
     for (ii = 0; ii<local_ncols;ii++){
       sendbuf[ii] = temp1[ NX +ii ];
     }
-    MPI_Send(sendbuf,4,MPI_INT,0,tag,MPI_COMM_WORLD);
-  }
 
+    // send data left and receive right
+
+    MPI_Sendrecv(sendbuf,4,MPI_INT,left,tag,
+                recvbuf,4,MPI_INT,right,tag,
+              MPI_COMM_WORLD,&status);
+    for (jj = 0; jj < local_ncols;jj++){
+      temp1[(local_nrows +1)*NX +jj] = recvbuf[jj];
+    }
+
+    // send data right receive left
+    MPI_Sendrecv(sendbuf,4,MPI_INT,right,tag,
+                recvbuf,4,MPI_INT,left, tag,
+                MPI_COMM_WORLD, &status);
+
+    for (jj = 0; jj < local_ncols;jj++){
+      temp1[jj] = recvbuf[jj];
+    }
+
+    // at this point all tanks have 3 rows so can do its calculation
+    // do calculation
+
+    for (jj = 0; jj <local_ncols;jj++){
+      temp1[NX +jj] = temp1[NX +jj] + temp1[jj] +temp1[NX*2 +jj];
+    }
+
+    // join grid again
+    for (ii = 0; ii< NX; ii++){
+      grid[rank*NX ]
+    }
+    if (rank == 0){
+      for(jj =0; jj<local_ncols; jj++){
+        printf("%3d ",temp1[NX +jj]);
+        gridfinal[jj] = temp1[NX +jj];
+      }
+      printf("\n");
+      for(int k= 1; k <size;k++){
+        MPI_Recv(recvbuf,4,MPI_INT,k,tag,MPI_COMM_WORLD,&status);
+      	for(jj=0;jj < 4;jj++) {
+      	  printf("%3d ",recvbuf[jj]);
+          gridfinal[k*NX +jj] = recvbuff[jj];
+      	}
+        printf("\n");
+      }
+      printf("\n");
+    }
+    else {
+      for (ii = 0; ii<local_ncols;ii++){
+        sendbuf[ii] = temp1[ NX +ii ];
+      }
+      MPI_Send(sendbuf,4,MPI_INT,0,tag,MPI_COMM_WORLD);
+    }
+}
   /* don't forget to tidy up whgedten we're done */
   MPI_Finalize();
   free(sendbuf);
@@ -120,7 +122,7 @@ int main(int argc, char* argv[]){
 
   for (ii = 0; ii < NX; ii++){
     for(jj = 0; jj <NY; jj++){
-      printf("%d\t",grid[ii*NX +jj]);
+      printf("%d\t",gridfinal[ii*NX +jj]);
     }
     printf("\n");
   }
